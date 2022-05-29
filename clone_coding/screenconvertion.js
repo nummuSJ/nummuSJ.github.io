e0272ca7bdf9cf6f5ba2aca0cbb7c5e25a910db2
nummuSJ <92517952+nummuSJ@users.noreply.github.com>
2022-05-29 15:29:05 +0900

    2022.5.29 중간저장

---
+++ clone_coding/screenconvertion.js
@@ -0,0 +1,6 @@
+var monitorbutton = document.querySelector('input.screenconvert1');
+var smartphonebutton = document.querySelector('input.screenconvert2');
+
+if(this.value == "screenconvert1"){
+  if(this.)
+}
