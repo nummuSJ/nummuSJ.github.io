66a272928f4eb440059189fbbed91ce6e78deed7
nummuSJ <92517952+nummuSJ@users.noreply.github.com>
2022-06-01 00:03:36 +0900

    2022.6.1
    팝업창 완벽하게 추가

--- clone_coding/jsfiles/popUp.js
+++ clone_coding/jsfiles/popUp.js
@@ -1,24 +1,54 @@
 $(document).ready(function(){
 
-  $('#menuimg').hover(function(){
-      $('#menuimg').css('transform','rotate(90deg)');
+  $('#menuimg1').hover(function(){
+      $('#menuimg1').css('transform','rotate(90deg)');
       $('#menuPopUp').css('opacity','1');
+      $('#menuPopUp').css('pointer-events','all');
     },function(){
-      $('#menuimg').css('transform','rotate(0deg)');
-      $('#menuPopUp').css('opacity','0');
+      $('#menuPopUp').css('pointer-events' , 'all');
+      setTimeout(function(){
+        if($('#menuPopUp').is(":hover") == true){
+          $('#menuPopUp').hover(function(){
+            $('#menuimg1').css('transform','rotate(90deg)');
+            $('#menuPopUp').css('opacity','1');
+            $('#menuPopUp').css('pointer-events','all');
+          },function(){
+            $('#menuimg1').css('transform','rotate(0deg)');
+            $('#menuPopUp').css('opacity','0');
+            $('#menuPopUp').css('pointer-events','none');
+            }
+          )
+        }else{
+          $('#menuimg1').css('transform','rotate(0deg)');
+          $('#menuPopUp').css('opacity','0');
+          $('#menuPopUp').css('pointer-events','none');
+        }
+      },100);
+
     }
+
   );
 
-  if($('#menuPopUp').css('opacity') >= '0.1'){
-    $('#menuPopUp').hover(function(){
-      $('#menuimg').css('transform','rotate(90deg)');
-      $('#menuPopUp').css('opacity','1');
-    },function(){
-      $('#menuimg').css('transform','rotate(0deg)');
-      $('#menuPopUp').css('opacity','0');
-      //작동안함 !! 팝업창에 마우스 올려서 팝업이 유지되어야 하는데 그렇게 안됌
-      }
-    );
-  }
+  // $('#menuPopUp').hover(function(){
+  //   $('#menuimg1').css('transform','rotate(90deg)');
+  //   $('#menuPopUp').css('opacity','1');
+  // },function(){
+  //   $('#menuimg1').css('transform','rotate(0deg)');
+  //   $('#menuPopUp').css('opacity','0');
+  //   //작동안함 !! 팝업창에 마우스 올려서 팝업이 유지되어야 하는데 그렇게 안됌
+  //   }
+  // );
+  //
+  // if($('#menuPopUp').css('opacity') == '1'){
+  //   $('#menuPopUp').hover(function(){
+  //     $('#menuimg1').css('transform','rotate(90deg)');
+  //     $('#menuPopUp').css('opacity','1');
+  //   },function(){
+  //     $('#menuimg1').css('transform','rotate(0deg)');
+  //     $('#menuPopUp').css('opacity','0');
+  //     //작동안함 !! 팝업창에 마우스 올려서 팝업이 유지되어야 하는데 그렇게 안됌
+  //     }
+  //   );
+  // }
 
 });
