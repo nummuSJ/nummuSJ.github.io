1cef5b348e5822cc7031f45f3f8d76973e7ab8cb
nummuSJ <92517952+nummuSJ@users.noreply.github.com>
2022-06-02 23:46:35 +0900

    2022.6.2
    슬라이드 이미지 페이드인아웃 완성
    (프로그래스 바 미완성)

--- clone_coding/jsfiles/slide.js
+++ clone_coding/jsfiles/slide.js
@@ -1,61 +1,11 @@
-var slide1 = $('#slide1');
-var slide2 = $('#slide2');
-var slide3 = $('#slide3');
-
 $(document).ready(function(){
 
-  setInterval("setTime()",0);
-  // var slide1 = $('#slide1');
-  // var slide2 = $('#slide2');
-  // var slide3 = $('#slide3');
+  $('#slideImage>ul>li').hide();
+  $('#slideImage>ul>li:first-child').show();
 
-  // setInterval(function(){
-  //   slide1.delay(1000);
-  //   slide1.css('opacity','0');
-  //   slide1.css('pointer-events','none');
-  //   slide2.css('opacity','1');
-  //   slide2.css('pointer-events','all');
-  //   slide3.css('opacity','0');
-  //   slide3.css('pointer-events','none');
-  //   slide2.delay(1000);
-  //   slide1.css('opacity','0');
-  //   slide1.css('pointer-events','none');
-  //   slide2.css('opacity','0');
-  //   slide2.css('pointer-events','none');
-  //   slide3.css('opacity','1');
-  //   slide3.css('pointer-events','all');
-  //   slide3.delay(1000);
-  //   slide1.css('opacity','1');
-  //   slide1.css('pointer-events','all');
-  //   slide2.css('opacity','0');
-  //   slide2.css('pointer-events','none');
-  //   slide3.css('opacity','0');
-  //   slide3.css('pointer-events','none');
-  // },0);
-
+  setInterval(function(){
+    $('#slideImage>ul>li:first-child').fadeOut(1000).next().delay(1100).fadeIn(1000).end().appendTo('#slideImage>ul');
+  },5000);
+  // fadeOut() 과 fadeIn은 시작과 동시에 다음 함수로 넘어간다.
+  // 고로 fadeOut(2000).fadeIn(1000) 할 시엔 반쯤 사라질때 다시 뜬다.
 });
-
-function setTime(){
-  slide1.delay(1000);
-  slide1.css('opacity','0');
-  slide1.css('pointer-events','none');
-  slide2.css('opacity','1');
-  slide2.css('pointer-events','all');
-  slide3.css('opacity','0');
-  slide3.css('pointer-events','none');
-  slide2.delay(1000);
-  slide1.css('opacity','0');
-  slide1.css('pointer-events','none');
-  slide2.css('opacity','0');
-  slide2.css('pointer-events','none');
-  slide3.css('opacity','1');
-  slide3.css('pointer-events','all');
-  slide3.delay(1000);
-  slide1.css('opacity','1');
-  slide1.css('pointer-events','all');
-  slide2.css('opacity','0');
-  slide2.css('pointer-events','none');
-  slide3.css('opacity','0');
-  slide3.css('pointer-events','none');
-}
-// 슬라이드 이벤트 덜 완성했음
