d004f30e0a1c464176d6cdc74cdfea5eb9f1f172
nummuSJ <92517952+nummuSJ@users.noreply.github.com>
2022-06-06 00:00:49 +0900

    2022.6.5
    프로그래스 바 진행 추가

--- clone_coding/jsfiles/slide.js
+++ clone_coding/jsfiles/slide.js
@@ -1,14 +1,20 @@
 $(document).ready(function(){
 
   $('#slideImage>ul>li').hide();
-  $('#slideImage>ul>li:first-child').show();
+  $('#slideImage>ul>li:first-child').fadeIn(500);
 
   setInterval(function(){
-    $('#progress div:odd').css("width","90px")
-  },0); // 선택자 공부해서 프로그래스 바 분할 조작 해야댐
+    $('#progressShow_1').animate({'width':"90px"},4500);
+    $('#progressShow_1').css("width","0px");
+  },1000); // 선택자 공부해서 프로그래스 바 분할 조작 해야댐
   setInterval(function(){
-    $('#slideImage>ul>li:first-child').fadeOut(1000).next().delay(1100).fadeIn(1000).end().appendTo('#slideImage>ul');
-  },5000);
+    $('#slideImage>ul>li:first-child').fadeOut(500).next().delay(500).fadeIn(500).end().appendTo('#slideImage>ul');
+  },5500);
+  setTimeout(function(){
+    setInterval(function(){
+      $('#progressBarBase >li:last-child').prependTo('#progressBarBase');
+    },5500);
+  },500);
   // fadeOut() 과 fadeIn은 시작과 동시에 다음 함수로 넘어간다.
   // 고로 fadeOut(2000).fadeIn(1000) 할 시엔 반쯤 사라질때 다시 뜬다.
 
