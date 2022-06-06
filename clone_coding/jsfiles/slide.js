e092c01889576e162c91dce7e0ad2dd54c1f855c
nummuSJ <92517952+nummuSJ@users.noreply.github.com>
2022-06-06 16:16:32 +0900

    2022.6.6
    프로그래스바 (클릭이벤트 제외) 완성

--- clone_coding/jsfiles/slide.js
+++ clone_coding/jsfiles/slide.js
@@ -4,9 +4,9 @@ $(document).ready(function(){
   $('#slideImage>ul>li:first-child').fadeIn(500);
 
   setInterval(function(){
-    $('#progressShow_1').animate({'width':"90px"},4500);
-    $('#progressShow_1').css("width","0px");
-  },1000); // 선택자 공부해서 프로그래스 바 분할 조작 해야댐
+    $('#progressShow_1').animate({'width':"100px"},5000).delay(500);
+    $('#progressShow_1').animate({"width":"0px"},0);
+  }); // 선택자 공부해서 프로그래스 바 분할 조작 해야댐
   setInterval(function(){
     $('#slideImage>ul>li:first-child').fadeOut(500).next().delay(500).fadeIn(500).end().appendTo('#slideImage>ul');
   },5500);
