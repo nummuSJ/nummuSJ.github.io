c16aedd1c171510c41941b842629bd0b34533fe4
nummuSJ <92517952+nummuSJ@users.noreply.github.com>
2022-06-10 23:30:23 +0900

    2022.6.10
    자동 슬라이드 학습

--- clone_coding/jsfiles/layout_2_slide.js
+++ clone_coding/jsfiles/layout_2_slide.js
@@ -1,6 +1,10 @@
 $(document).ready(function(){
+  // setInterval(function(){
+  //   $('#layout_2_slide > ul > li').animate({"right":"310px"},2000).delay(2000);
+  //   $('#layout_2_slide > ul > li:first-child').appendTo('#layout_2_slide > ul');
+  // },0);
   setInterval(function(){
-    $('#layout_2_slide > ul > li:first-child').appendTo('#layout_2_slide > ul');
-
-  },1000);
+    $('#layout_2_slide > ul > li').css('transform','translate(-310px)');
+    $('#layout_2_slide > ul > li:last-child').prependTo('#layout_2_slide > ul');
+  },2000)
 });
